d4d7a7ed72cf9b15aa15a9067c79006f16ac0220
jonycosta <costavisor@gmail.com>
2026-03-06 13:04:26 +0100

    Publicacion Automatica JD Builders

--- deploy_fast.bat
+++ deploy_fast.bat
@@ -25,7 +25,7 @@ echo.
 
 "%GIT_PATH%" add .
 "%GIT_PATH%" commit -m "Publicacion Automatica JD Builders"
-"%GIT_PATH%" push origin main
+"%GIT_PATH%" push origin master
 
 echo.
 echo ==========================================
